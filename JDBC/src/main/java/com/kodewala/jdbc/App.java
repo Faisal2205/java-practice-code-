b8a14f920845d89cd1cf71bf85d4b56863bb05d7
Mohd Faisal <mohdfaisal2216@gmail.com>
2025-06-09 17:17:51 +0530

    Spring Data(Hibernate) & Spring MVC Web

--- JDBC/src/main/java/com/kodewala/jdbc/App.java
+++ JDBC/src/main/java/com/kodewala/jdbc/App.java
@@ -2,6 +2,6 @@ package com.kodewala.jdbc;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Jdbc Connection!!!!!");
+    System.out.println("Hello World!");
   }
 }
